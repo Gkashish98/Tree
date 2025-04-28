c05ff2155911164c63931587ad8b2ba9bd30852a
Gkashish98 <148377607+Gkashish98@users.noreply.github.com>
2025-04-28 22:57:08 +0530

    Create Average of Levels in Binary Tree.cpp

---
+++ Average of Levels in Binary Tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root)
+    {
+       vector<vector<int>>ans;
+       if(root==NULL)
+       {
+         return ans;
+       }
+       queue<TreeNode*>q;
+       q.push(root);
+       while(!q.empty())
+       {
+          int n=q.size();
+          vector<int>level;
+          for(int i=0;i<n;i++)
+          {
+            TreeNode* node=q.front();
+            q.pop();
+            if(node->left!=NULL)
+            {
+                q.push(node->left);
+            }
+            if(node->right!=NULL)
+            {
+                q.push(node->right);
+            }
+             level.push_back(node->val);
+          }
+          ans.push_back(level);
+       }
+       return ans;
+    }
+    vector<double> averageOfLevels(TreeNode* root) 
+    {
+       vector<vector<int>>v=levelOrder(root);
+       vector<double>result;
+       for(int i=0;i<v.size();i++)
+       {
+          vector<int>ele=v[i];
+          int s=ele.size();
+          double sum=0;
+          for(int j=0;j<ele.size();j++)
+          {
+             sum+=ele[j];
+          }
+          result.push_back(sum/s);
+       }
+        return result;
+    }
+};
